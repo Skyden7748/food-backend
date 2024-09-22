76b5576b4fae38df247aec4d270ef944a30ac6f1
Skyden7748 <127587527+Skyden7748@users.noreply.github.com>
2024-09-22 21:28:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect ("mongodb+srv://skyden69:S2IzWwlt74Ym1qZU@cluster0.bjwzg.mongodb.net/food-del")
+    await mongoose.connect ("")
     .then(()=>console.log("MongoDB connected"))
 };
